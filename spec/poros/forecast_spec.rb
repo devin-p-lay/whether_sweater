fb32a53f099a662b4f89240cbbdceea7f5984b13
Devin Pile <devinpile@gmail.com>
2022-03-06 01:05:44 -0700

    Add hourly forecast poro and test refactor

--- spec/poros/forecast_spec.rb
+++ spec/poros/forecast_spec.rb
@@ -5,7 +5,7 @@ describe Forecast do
     data = OpenWeatherService.weather_data(39.738453, -104.984853)
     forecast = Forecast.new(data)
 
-    expect(forecst).to be_a Forecast
+    expect(forecast).to be_a Forecast
     expect(forecast.current).to be_a CurrentForecast
     expect(forecast.hourly.first).to be_an HourlyForecast
     expect(forecast.daily.first).to be_a DailyForecast
