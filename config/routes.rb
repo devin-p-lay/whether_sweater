6bcda5d101d82335f27e41bd3ad1ebac08c66cae
Devin Pile <devinpile@gmail.com>
2022-03-07 10:46:26 -0700

    Add get books route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :forecast, only: :index
       resources :background, only: :index
+      get '/book-search', to: 'books#search'
     end
   end
 end
