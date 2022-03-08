7c1096cc2aba820dd908151800b268ea5ccf8c35
Devin Pile <devinpile@gmail.com>
2022-03-08 10:17:10 -0700

    Add activity request spec

---
+++ spec/requests/api/v1/activity_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'activities endpoint' do
+  it 'retrieves activities for a given destination' do
+    get '/api/v1/activitivites' , params: { destination: 'denver,co' }
+
+    expect(response).to be_successful
+
+    activities = JSON.parse(response.body, symbolize_names: true)
+
+    expect(activities).to be a Hash
+    expect(activities).to have_key(:data)
+    expect(activities[:data]).to have_key(:id)
+    expect(activities[:data]).to have_key(:type)
+    expect(activities[:data]).to have_key(:attributes)
+  end
+
+  it 'returns an error if no destination is given' do
+    get '/api/v1/activities'
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq 400
+  end
+
+  it 'return an error if destination does not exist' do
+    get '/api/v1/activitivites' , params: { destination: 'asdfjkl' }
+
+    expect(response).to_not be_successful
+  end
+end 
